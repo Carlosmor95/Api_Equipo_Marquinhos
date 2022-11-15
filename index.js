97d74f1da4f0bb90f83651222e7fff667e97ffbf
A01236054 <a01236054@itesm.mx>
2022-11-15 09:56:10 -0600

    Pendiente pruebas en Firebase

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express')
+var admin = require("firebase-admin");
 
 const app = express()
+
+
+// NOTA: Es necesario tener este archivo para acceder a base de datos
+var serviceAccount = require("./FirebaseKey"); 
+
+// Firebase
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://lsm-john-deere-default-rtdb.firebaseio.com"
+});
+
+var database = admin.database();
+var ref = database.ref("referencia/de/documento/inaccesible/con/Javascrip/en/el/cliente");
+ref.once("value", function(snapshot) {
+console.log("El dato es", snapshot.val());
+});
 
 
 app.get('/health', function (req, res) {
